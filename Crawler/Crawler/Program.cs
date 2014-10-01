1ef6816547c1cb14d7756bf917c7a35dfea3fc21
Casper Laustsen <casper.laustsen@gmail.com>
2014-10-01 11:07:48 +0200

    first push of project

---
+++ Crawler/Crawler/Program.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Globalization;
+using System.Web;
+using System.Collections.Generic;
+
+namespace Crawler {
+    class Program {
+        static void Main(string[] args) {
+            string initialPage = "http://www.oprah.com";
+
+            
+            Console.ReadLine();
+            //CrawlPages(initialPage);
+            indexer index = new indexer();
+
+        }
+
+        static void CrawlPages(string initialPage) {
+
+            string webpage = "";
+            string currentMainPage = "";
+            string tempPage = "";
+            string url;
+            string textToMatch = "href=\"([^\"]*)";
+            Match match;
+            List<string> visitedPages = new List<string>();
+            Queue pageQueue = new Queue();
+
+            string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\pagefolder\";
+
+            int counter = 0;
+
+            visitedPages.Add(initialPage);
+            pageQueue.Enqueue(initialPage);
+
+            Regex regex = new Regex(textToMatch, RegexOptions.IgnoreCase);
+
+            while (pageQueue.Count != 0) {
+                if (counter == 5) {
+                    break;
+                }
+                while (webpage == "") {
+                    currentMainPage = (string)pageQueue.Peek();
+                    webpage = FetchWebPage((string)pageQueue.Dequeue());
+                }
+
+                match = regex.Match(webpage);
+
+                while (match.Success) {
+                    url = match.Value;
+                    if (url.EndsWith(".css") == true) {
+                        match = match.NextMatch();
+                    } else {
+                        url = NormalizeUrl(url, tempPage, currentMainPage);
+
+                        // if (url.EndsWith(".dk") || url.Contains(".dk/")){
+
+                        if (!visitedPages.Contains(url)) {
+                            visitedPages.Add(url);
+                            pageQueue.Enqueue(url);
+                            Console.WriteLine(url);
+                        }
+                        //}
+                        match = match.NextMatch();
+                    }
+                }
+
+                webpage = removeHead(webpage);
+                webpage = removeScriptAndStyle(webpage);
+                webpage = saveWebpage(webpage);
+
+                // Write the stream contents to a new file
+                using (StreamWriter outfile = new StreamWriter(mydocpath + RemoveSpecialCharacters(currentMainPage) + ".txt")) {
+                    outfile.WriteLine(currentMainPage);
+                    outfile.Write(webpage.ToString());
+                    outfile.Close();
+                }
+
+                webpage = "";
+
+                System.Threading.Thread.Sleep(1500);
+
+                counter++;
+            }
+            return;
+        }
+
+        static string NormalizeUrl(string url, string tempPage, string currentMainPage) {
+
+            string webpageToMatch = "http://[a-zA-Z0-9.-]*";
+            Regex currentUrlRegex = new Regex(webpageToMatch, RegexOptions.IgnoreCase);
+            Match mainPageMatch;
+
+            url = url.Remove(0, 6).TrimEnd('/');
+
+
+            mainPageMatch = currentUrlRegex.Match(url);
+            tempPage = mainPageMatch.Value;
+
+            if (url.StartsWith("//")) {
+                url = url.Remove(0, 2);
+            }
+            if (url.StartsWith("/") == true) {
+                url = currentMainPage + url;
+
+            }
+            if (url.StartsWith("http://www.") == false) {
+                if (url.StartsWith("http://") == true) {
+                    url.Insert(8, "www.");
+                } else if (url.StartsWith("www.") == true) {
+                    url = "http://" + url;
+                } else if (url.StartsWith("www.") == false) {
+                    url = "http://www." + url;
+                }
+            }
+
+            // Less important?
+            url = url.Substring(0, tempPage.Length).ToLower() + url.Substring(tempPage.Length);
+
+            for (int i = 0; i < url.Length - 3; i++) {
+                if (url[i] == '%') {
+                    url = url.Substring(0, i + 1) + url.Substring(i + 1, 2).ToUpper() + url.Substring(i + 3, url.Length - 3 - i);
+                }
+            }
+
+            return url;
+        }
+
+
+        static string saveWebpage(string webpage) {
+            const string htmlCode = "<.*?>";
+            webpage = Regex.Replace(webpage, htmlCode, string.Empty);
+            return webpage;
+        }
+
+        static string removeScriptAndStyle(string webpage) {
+            var regex = new Regex("(\\<script(.+?)\\</script\\>)|(\\<style(.+?)\\</style\\>)", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+            return regex.Replace(webpage, "");
+        }
+
+        static string removeHead(string webpage) {
+            Regex rRemScript = new Regex(@"<head [^>]*>[\s\S]*?</head>");
+            return rRemScript.Replace(webpage, "");
+        }
+        public static string RemoveSpecialCharacters(string str) {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in str) {
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_') {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+
+        static string FetchWebPage(string url) {
+            string txt = "";
+
+            Console.WriteLine("LOADING URL: " + url);
+
+            try {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = null;
+                response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK) {
+                    Stream responseStream = response.GetResponseStream();
+                    StreamReader reader;
+                    reader = new StreamReader(responseStream, Encoding.GetEncoding(response.CharacterSet));
+                    txt = reader.ReadToEnd();
+                    response.Close();
+                    reader.Close();
+                }
+
+            } catch (Exception e) {
+                txt = "";
+            }
+
+            return txt;
+        }
+
+        static bool RobotViolalationCheck(string url) {
+
+            List<string> disallows = new List<string>();
+            bool violation = false;
+            int i = url.IndexOf(".dk/") + 4;
+            string robotsUrl = url.Substring(0, i) + "robots.txt";
+            disallows = RobotParser(robotsUrl, 1);
+            foreach (string rule in disallows) {
+                if (rule == url) {
+                    violation = true;
+                }
+            }
+
+            return violation;
+        }
+
+        static List<string> RobotParser(string url, int ruleType) {
+            string userAgentName = "Treeline";
+            List<string> disallows = new List<string>();
+            List<string> allows = new List<string>();
+            List<string> organizedTxt = new List<string>();
+
+            organizedTxt = FetchRobotsTxt(url);
+            disallows = FindRobotRules(organizedTxt, userAgentName, 1);
+            allows = FindRobotRules(organizedTxt, userAgentName, 2);
+            /*Console.WriteLine("Disallows:");
+            PrintRobotRules(disallows);
+            Console.WriteLine("Allows:");
+            PrintRobotRules(allows); */
+
+            return disallows;
+        }
+
+        static void PrintRobotRules(List<string> rules) {
+            foreach (string rule in rules) {
+                Console.WriteLine(rule);
+            }
+            Console.ReadLine();
+        }
+
+        static List<string> FindRobotRules(List<String> organizedTxt, string userAgentName, int ruleType) {
+            List<string> rules = new List<string>();
+
+            for (int i = 0; i < organizedTxt.Count - 1; i++) {
+                if (organizedTxt[i] == userAgentName) {
+                    rules = saveRobotRules(organizedTxt, i, ruleType);
+                    break;
+                } else if (organizedTxt[i] == "*") {
+                    rules = saveRobotRules(organizedTxt, i, ruleType);
+                } else { }
+            }
+            return rules;
+        }
+
+        static List<string> saveRobotRules(List<string> organizedTxt, int i, int ruleType) {
+            List<string> rules = new List<string>();
+            int j = 1;
+
+            string ruleName = GetRobotRuleName(ruleType);
+
+            while (j != 0 && i + j < organizedTxt.Count - 1) {
+                if (organizedTxt[i + j] == "User-agent:") {
+                    j = 0;
+                } else if (organizedTxt[i + j] == ruleName) {
+                    rules.Add(organizedTxt[i + j + 1]);
+                    j += 2;
+                } else {
+                    j += 2;
+                }
+            }
+
+            return rules;
+        }
+
+        static string GetRobotRuleName(int ruleType) {
+            string ruleName;
+            switch (ruleType) {
+                case 1:
+                    ruleName = "Disallow:";
+                    break;
+
+                case 2:
+                    ruleName = "Allow:";
+                    break;
+
+                default:
+                    ruleName = "INVALID";
+                    break;
+            }
+            return ruleName;
+        }
+
+        static List<string> FetchRobotsTxt(string url) {
+            string txt = "";
+            List<string> organizedTxt = new List<string>();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK) {
+                Stream responseStream = response.GetResponseStream();
+                StreamReader reader;
+
+                reader = new StreamReader(responseStream, Encoding.GetEncoding(response.CharacterSet));
+                txt = reader.ReadToEnd();
+                response.Close();
+                reader.Close();
+
+                organizedTxt = txt.Split(null).ToList();
+            }
+
+            return organizedTxt;
+        }
+    }
+}
---
+++ Crawler/Crawler/indexer.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace Crawler {
+    class indexer {
+        List<string> wordsNoDuplicates = new List<string>();
+        List<int> timesSeen = new List<int>();
+        string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\pagefolder\";
+        public Dictionary<string, LinkedList<int>> invertedIndex = new Dictionary<string, LinkedList<int>>();
+
+        public indexer() {
+
+
+            StringBuilder sb = new StringBuilder();
+            List<string> myStrings = new List<string>();
+            string[] dirs = Directory.GetFiles(mydocpath);
+
+            foreach (string dir in dirs) {
+                Console.WriteLine(dir);
+                if (dir.ToString() == "data.txt") {
+
+                } else {
+                    using (StreamReader sr = new StreamReader(dir)) {
+                        String line;
+                        // Read and display lines from the file until the end of 
+                        // the file is reached.
+                        while ((line = sr.ReadLine()) != null) {
+                            sb.AppendLine(line);
+                        }
+                    }
+                    string allines = sb.ToString();
+                    myStrings.Add(allines);
+                    sb.Clear();
+                }
+            }
+
+
+            List<string[]> result = new List<string[]>();
+
+            foreach (string content in myStrings) {
+                char[] delimiters = new char[] { ' ' };
+                string[] res = content.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < res.Count(); i++) {
+                    res[i] = Regex.Replace(res[i], @"^\s+$[\r\n]*", "", RegexOptions.Multiline);
+                    res[i] = Regex.Replace(res[i], @"\t|\n|\r", "");
+                }
+
+
+                result.Add(res);
+            }
+            StreamReader counter = new StreamReader(mydocpath + "data.txt");
+            int urlCounter = Convert.ToInt32(counter.ReadLine());
+
+            List<string> cleaningBlanks = new List<string>();
+            foreach (string[] stringArray in result) {
+                cleaningBlanks.Clear();
+
+                foreach (string word in stringArray) {
+                    if (word != "") {
+                        cleaningBlanks.Add(word);
+                        invertedIndexMethod(word, urlCounter);
+                    }
+                }
+                indexFreqTable(cleaningBlanks);
+                Console.WriteLine(urlCounter);
+                using (StreamWriter outfile = new StreamWriter(mydocpath + "freqTable" + urlCounter + ".txt")) {
+                    
+                    for (int i = 0; i < wordsNoDuplicates.Count(); i++) {
+                        outfile.WriteLine(wordsNoDuplicates.ElementAt(i) + " " + timesSeen.ElementAt(i));
+
+                    }
+                    urlCounter++;
+                    wordsNoDuplicates.Clear();
+                    timesSeen.Clear();
+                    outfile.Close();
+                }
+            }
+
+            saveInvertedIndex();
+        }
+
+
+        public void invertedIndexMethod(string word, int docId) {
+            if(invertedIndex.ContainsKey(word)) {
+                if (!(invertedIndex[word].Last() == docId)) {
+                    invertedIndex[word].AddLast(docId);
+                }
+            } else {
+            invertedIndex.Add(word, new LinkedList<int>());
+            invertedIndex[word].AddLast(docId);
+            }
+        }
+
+
+        public void indexFreqTable(List<string> input) {
+
+            input.RemoveAt(0);
+            int index;
+
+            foreach (string word in input) {
+                if (wordsNoDuplicates.Contains(word)) {
+                    index = wordsNoDuplicates.IndexOf(word);
+                    timesSeen.Insert(index, timesSeen.ElementAt(index) + 1);
+                } else {
+                    wordsNoDuplicates.Add(word);
+                    timesSeen.Add(1);
+                }
+
+            }
+        }
+
+        public void saveInvertedIndex() {
+            using (StreamWriter outfile = new StreamWriter(mydocpath + "invertedIndex.txt")) {
+                foreach (KeyValuePair<string, LinkedList<int>> res in invertedIndex) {
+                    string lldocIds = "";
+                    foreach(int i in res.Value) {
+                        lldocIds += i.ToString() + ',';
+                    }
+
+                    outfile.WriteLine(res.Key + " " + lldocIds);
+                }
+            }
+        }
+
+        //read files
+        //create/update dictionary
+        //add word frequency
+        public void saveIndex() {
+
+            using (StreamWriter outfile = new StreamWriter(mydocpath + "result.txt")) {
+                for (int i = 0; i < wordsNoDuplicates.Count(); i++) {
+                    outfile.WriteLine(wordsNoDuplicates.ElementAt(i) + " " + timesSeen.ElementAt(i));
+
+                }
+                outfile.Close();
+            }
+
+        }
+
+    }
+}
